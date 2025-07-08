84f6e0df36a5d545c5141b2072d202cde81e7b72
Michael Rubinstein <mike.rubinstein@gmail.com>
2025-07-08 17:34:00 -0500

    Create currentTime.go

---
+++ currentTime.go
@@ -0,0 +1,15 @@
+package main
+   
+import (
+    "fmt"
+    "time"
+)
+   
+func main() {
+   
+    // get date and time and store in variable on left
+    currentTime := time.Now()
+   
+    // printing the date and time in string format
+    fmt.Println("Current Time in String: ", currentTime.String())
+}
